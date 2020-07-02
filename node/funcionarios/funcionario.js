37e6707b536f1f95dde00ead75b24dc535c26031
Codleo <leo.santos2001@hotmail.com>
2020-07-01 21:50:57 -0300

    master

--- node/funcionarios/funcionario.js
+++ node/funcionarios/funcionario.js
@@ -1,7 +1,17 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios = require('axios')
 
+// Desafio a chinesa com menor salario  e mulher
+const apenasChinesa = funcionarios => funcionarios.pais === 'China'
+const mulheres = f => f.genero === 'F'
+const menorSalario = (func,funcAtual) => {
+    return func.salario < funcAtual.salario ? func : funcAtual
+}
 axios.get(url).then(response => {
     const funcionarios = response.data
-    console.log(funcionarios)
+    //console.log(funcionarios)
+
+const func = funcionarios.filter(apenasChinesa).filter(mulheres).reduce(menorSalario)
+
+console.log(func)
 })
